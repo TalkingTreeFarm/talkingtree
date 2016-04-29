1e94175afee27bab9a8744532eaa6b4af47d0b56
rrapstine <richard.rapstine@gmail.com>
2016-04-29 15:41:14 -0500

    Further JS updates for products.main. Began order summary section

--- app/views/product/main.blade.php
+++ app/views/product/main.blade.php
@@ -5,9 +5,6 @@
 @stop
 
 @section('content')
-    @foreach($products as $product)
-        <input id="{{{ $product['name'] }}}" type="hidden" value="{{{ $product['amount'] }}}">
-    @endforeach
     <div class="container">
 
         <div class="row">
@@ -30,38 +27,45 @@
             <div class="col-lg-3">
                 <div class="form-group">
                     <label for="size">Size*</label>
-                    <select class="form-control">
+                    <select id="size" name="size" class="form-control">
                         <option value="0" selected>Select Size</option>
 
                         @foreach($products as $product)
                             @if($product->visible)
-                                <option value="{{{ $product->id }}}">{{{ $product->name }}} - ${{{ $product->price }}}</option>
+                                <option id="{{{ str_replace(" ", "-", $product->name) }}}" value="{{{ $product->id }}}" data-amount="{{{ $product->amount }}}">{{{ $product->name }}} - ${{{ $product->price }}}</option>
                             @endif
                         @endforeach
                     </select>
 
+                    @foreach($products as $product)
+                        @if($product->name == "Eggs")
+                            <!-- Hidden Attribute For Eggs -->
+                            <input id="Eggs" type="hidden" data-amount="{{{ $product->amount }}}"></input>
+                        @endif
+                    @endforeach
+
                     <label for="quantity">Quantity*</label>
                     <div class="input-group form-group-options quantity-wrapper">
-                        <span  class="input-group-addon input-group-addon-remove quantity-remove btn">
-                            <span id="basket-sub" class="glyphicon glyphicon-minus"></span>
+                        <span id="basket-sub" class="input-group-addon input-group-addon-remove quantity-remove btn">
+                            <span class="glyphicon glyphicon-minus"></span>
                         </span>
 
                         <input id="quantity-baskets" type="text" name="quantity" class="form-control quantity-count" placeholder="1">
 
-                        <span class="input-group-addon input-group-addon-remove quantity-add btn">
-                            <span id="basket-add" class="glyphicon glyphicon-plus"></span>
+                        <span id="basket-add" class="input-group-addon input-group-addon-remove quantity-add btn">
+                            <span class="glyphicon glyphicon-plus"></span>
                         </span>
                     </div>
 
                     <label for="eggs">Eggs (By Dozen)</label>
                     <div class="input-group form-group-options quantity-wrapper">
-                        <span  class="input-group-addon input-group-addon-remove quantity-remove btn">
+                        <span id="egg-sub" class="input-group-addon input-group-addon-remove quantity-remove btn">
                             <span class="glyphicon glyphicon-minus"></span>
                         </span>
 
-                        <input type="text" name="quantity" class="form-control quantity-count" placeholder="1">
+                        <input id="quantity-eggs" type="text" name="quantity" class="form-control quantity-count" placeholder="0">
 
-                        <span class="input-group-addon input-group-addon-remove quantity-add btn">
+                        <span id="egg-add" class="input-group-addon input-group-addon-remove quantity-add btn">
                             <span class="glyphicon glyphicon-plus"></span>
                         </span>
                     </div>
@@ -74,7 +78,42 @@
                     </select>
                 </div>
             </div>
+            <div class="col-lg-3 col-lg-offset-2">
+                <div class="well">
+                    <ul class="order-summary">
+                        <li class="summary-item">
+                            <span class="item-desc">Basket:</span>
+                            <span id="basket-type" class="item-amount"></span>
+                        </li>
+                        <li class="summary-item">
+                            <span class="item-desc">Quantity:</span>
+                            <span id="basket-sum" class="item-amount"></span>
+                        </li>
+                        <li class="summary-item">
+                            <span class="item-desc">Eggs:</span>
+                            <span id="egg-sum" class="item-amount"></span><br>
+                            <span class="item-disclaimer">*By the dozen</span>
+                        </li>
+                        <li class="summary-item">
+                            <span class="item-desc">Delivery Method:</span>
+                            <span id="delivery-method" class="item-amount"></span><br>
+                            <span class="item-disclaimer text-hide">*This delivery method requires Paypal</span>
+                        </li>
+                        <li class="summary-item">
+                            <span class="item-desc">Order Total:</span>
+                            <span id="order-sum" class="item-amount"></span><br>
+                            <span class="item-disclaimer">*Taxes may apply</span>
+                        </li>
+                    </ul>
+                </div>
+
+                <button type="submit" name="checkout" class="btn btn-success col-lg-12">Proceed To Checkout</button>
+            </div>
         </div>
 
     </div>
+@stop
+
+@section('bottom-script')
+    <script type="text/javascript" src="assets/js/products.js"></script>
 @stop
--- public/assets/js/products.js
+++ public/assets/js/products.js
@@ -1,18 +1,52 @@
 (function() {
     // Variables
-    var $baskets = $('#quantity-baskets').value();
-    var $eggs    = $('#quantity-eggs').value();
+    var $baskets = Number($('#quantity-baskets').val());
+    var $eggs    = Number($('#quantity-eggs').val());
+
+    // Reset Quantities on Change
+    $('#size').change(function() {
+        $('#quantity-baskets').val(0);
+    });
 
     // Button Controls (Baskets)
     $('#basket-sub').on('click', function() {
-        if($baskets >= 0) {
+        if($baskets > 0) {
             $baskets -= 1;
         }
+
+        $('#quantity-baskets').val($baskets);
+        $('#basket-sum').text($baskets);
     });
 
     $('#basket-add').on('click', function() {
-        if($baskets < $('#')) {
+        var $selected = $('#size').find(':selected');
+
+        if($baskets < $selected.data('amount')) {
             $baskets += 1;
         }
+
+        $('#quantity-baskets').val($baskets);
+        $('#basket-sum').text($baskets);
+    });
+
+    // Button Controls (Eggs)
+    $('#egg-sub').on('click', function() {
+        if($eggs > 0) {
+            $eggs -= 1;
+        }
+
+        $('#quantity-eggs').val($eggs);
+        $('#egg-sum').text($eggs);
+    });
+
+    $('#egg-add').on('click', function() {
+        var $selected = $('#Eggs').data('amount');
+
+        if($eggs < $selected) {
+            $eggs += 1;
+        }
+
+        $('#quantity-eggs').val($eggs);
+        $('#egg-sum').text($eggs);
     });
 }());
