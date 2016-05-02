ab0aba5bec41e5ea58273278feb2756defce2ec1
stanholzendorf <sholzendorf@mail.com>
2016-05-02 18:14:55 -0500

    ADJUSTED PARTIAL NAVBAR / CREATED TESTUSER

--- app/views/partials/navbar.blade.php
+++ app/views/partials/navbar.blade.php
@@ -17,7 +17,7 @@
                     <li><a href="/events">Events</a></li>
                     <li><a href="{{{action('ProductsController@index')}}}">Order a Basket</a></li>
                     <li><a href="#footer">Contact</a></li>
-                    @if (Auth::user()->role_id==1)
+                    @if (Auth::check() && Auth::user()->isAdmin())
                         <li>
                             <a id="drop" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{{ Auth::user()->first_name . '\'s Profile' }}}<span class="caret"></span></a>
                             <ul id="menu" class="dropdown-menu" aria-labelledby="drop">
@@ -28,7 +28,7 @@
                                 <li><a href="{{{ action('UsersController@edit', Auth::user()->id) }}}"><i class="fa fa-pencil" aria-hidden="true"></i></i>&nbsp;Edit Profile</a></li>
                                 <li><a href="{{{ action('UsersController@getLogout', Auth::user()->id) }}}"><i class="fa fa-sign-out" aria-hidden="true"></i>&nbsp;Logout</a></li>
                             </ul> 
-                    @elseif (Auth::user()->role_id==2)                         
+                    @elseif (Auth::check())                         
                             <a id="drop" href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">{{{ Auth::user()->first_name . '\'s Profile' }}}<span class="caret"></span></a>
                             <ul id="menu" class="dropdown-menu" aria-labelledby="drop">
                                 <li><a href="{{{ action('OrdersController@index', Auth::user()->id) }}}"><i class="fa fa-shopping-cart"></i>&nbsp;My Orders</a></li>
