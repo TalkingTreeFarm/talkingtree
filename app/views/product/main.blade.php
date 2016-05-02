2ed7511cd2f372281ede319c9c7eec9f0d52c543
rrapstine <richard.rapstine@gmail.com>
2016-05-02 09:47:13 -0500

    Shopping cart front-end finished...I think

--- app/views/product/main.blade.php
+++ app/views/product/main.blade.php
@@ -22,17 +22,19 @@
                 @for($i = 0; $i < 9; $i++)
                     <img src="http://fillmurray.com/80/80" alt="Produce Image" />
                 @endfor
+
+                <p>*Required</p>
             </div>
             <!-- Selection Dropdowns -->
             <div class="col-lg-3">
-                <div class="form-group">
+                <form class="form-group" action="" method="post">
                     <label for="size">Size*</label>
                     <select id="size" name="size" class="form-control">
                         <option value="0" selected>Select Size</option>
 
                         @foreach($products as $product)
                             @if($product->visible)
-                                <option id="{{{ str_replace(" ", "-", $product->name) }}}" value="{{{ $product->id }}}" data-amount="{{{ $product->amount }}}">{{{ $product->name }}} - ${{{ $product->price }}}</option>
+                                <option id="{{{ str_replace(" ", "-", $product->name) }}}" value="{{{ $product->id }}}" data-amount="{{{ $product->amount }}}" data-price="{{{ $product->price }}}">{{{ $product->name }}} - ${{{ $product->price }}}</option>
                             @endif
                         @endforeach
                     </select>
@@ -40,7 +42,7 @@
                     @foreach($products as $product)
                         @if($product->name == "Eggs")
                             <!-- Hidden Attribute For Eggs -->
-                            <input id="Eggs" type="hidden" data-amount="{{{ $product->amount }}}"></input>
+                            <input id="Eggs" type="hidden" data-amount="{{{ $product->amount }}}" data-price="{{{ $product->price }}}"></input>
                         @endif
                     @endforeach
 
@@ -50,7 +52,7 @@
                             <span class="glyphicon glyphicon-minus"></span>
                         </span>
 
-                        <input id="quantity-baskets" type="text" name="quantity" class="form-control quantity-count" placeholder="1">
+                        <input id="quantity-baskets" type="text" name="quantity" class="form-control quantity-count" value="1">
 
                         <span id="basket-add" class="input-group-addon input-group-addon-remove quantity-add btn">
                             <span class="glyphicon glyphicon-plus"></span>
@@ -63,7 +65,7 @@
                             <span class="glyphicon glyphicon-minus"></span>
                         </span>
 
-                        <input id="quantity-eggs" type="text" name="quantity" class="form-control quantity-count" placeholder="0">
+                        <input id="quantity-eggs" type="text" name="quantity" class="form-control quantity-count" value="0">
 
                         <span id="egg-add" class="input-group-addon input-group-addon-remove quantity-add btn">
                             <span class="glyphicon glyphicon-plus"></span>
@@ -71,37 +73,38 @@
                     </div>
 
                     <label for="delivery">Delivery Method*</label>
-                    <select class="form-control">
+                    <select id="delivery" name="delivery" class="form-control">
                         <option value="0" selected>Select Method</option>
                         <option value="1">Pickup</option>
                         <option value="2">Home Delivery</option> <!-- Add note to summary with Paypal disclaimer -->
                     </select>
-                </div>
+                </form>
             </div>
-            <div class="col-lg-3 col-lg-offset-2">
+            <!-- Order Summary -->
+            <div class="col-lg-4 col-lg-offset-2">
                 <div class="well">
                     <ul class="order-summary">
                         <li class="summary-item">
                             <span class="item-desc">Basket:</span>
-                            <span id="basket-type" class="item-amount"></span>
+                            <span id="basket-type" class="item-amount">Not Selected</span>
                         </li>
                         <li class="summary-item">
                             <span class="item-desc">Quantity:</span>
-                            <span id="basket-sum" class="item-amount"></span>
+                            <span id="basket-sum" class="item-amount">1</span>
                         </li>
                         <li class="summary-item">
                             <span class="item-desc">Eggs:</span>
-                            <span id="egg-sum" class="item-amount"></span><br>
+                            <span id="egg-sum" class="item-amount">0</span><br>
                             <span class="item-disclaimer">*By the dozen</span>
                         </li>
                         <li class="summary-item">
                             <span class="item-desc">Delivery Method:</span>
-                            <span id="delivery-method" class="item-amount"></span><br>
-                            <span class="item-disclaimer text-hide">*This delivery method requires Paypal</span>
+                            <span id="delivery-method" class="item-amount">Not Selected</span><br>
+                            <span id="delivery-disclaimer" class="item-disclaimer text-hide">*This delivery method requires Paypal</span>
                         </li>
                         <li class="summary-item">
                             <span class="item-desc">Order Total:</span>
-                            <span id="order-sum" class="item-amount"></span><br>
+                            <span id="order-sum" class="item-amount">0</span><br>
                             <span class="item-disclaimer">*Taxes may apply</span>
                         </li>
                     </ul>
