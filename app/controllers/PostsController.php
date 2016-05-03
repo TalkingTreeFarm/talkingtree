aa134ccc2ef7cb6e667cbd6cd6ce76a1e5d5130c
stanholzendorf <sholzendorf@mail.com>
2016-05-03 15:05:59 -0500

    PUSH

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -29,7 +29,7 @@ class PostsController extends \BaseController {
 			$posts=Post::where('category_id', '=', Input::get('category_id'))->get();
 		} else {
 
-			$posts = Post::all()
+			$posts = Post::all();
 		}
 		return View::make('posts.main')->with('posts', $posts);
 	}
