b191928f0d290b688dc55591542cbf1dfc657774
Gaston Lenotre <gastonlenotre@gmail.com>
2016-05-10 14:15:05 -0500

    add phone# to footer

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -5,7 +5,7 @@
     	<div id="footer">
       © 2016 Talking Tree Farm
       <br>
-		San Antonio TX, 78109 | Like us on <a href="https://www.facebook.com/Talkingtreefarmpermaculture/"><i class="fa fa-facebook-official" aria-hidden="true"></i></a> | 210-123-4567 
+		San Antonio TX, 78109 | Like us on <a href="https://www.facebook.com/Talkingtreefarmpermaculture/"><i class="fa fa-facebook-official" aria-hidden="true"></i></a> | 210-355-7447 
       <br>
       Designed by <a class="" href="https://github.com/stanholzendorf">Stan Holzendorf </a> | <a class="" href="http://gastonlenotre.org/">Gaston Lenotre </a> | <a class="" href="http://doublerdevelopment.com/">Richard Rapstine </a>
        </div>
