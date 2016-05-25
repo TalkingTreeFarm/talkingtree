b96e6e917a23b8933fafe9cdffdf71c1d6459dba
stanholzendorf <sholzendorf@mail.com>
2016-05-25 14:50:31 -0500

    Reminder and Reset Password

--- app/views/password/reset.blade.php
+++ app/views/password/reset.blade.php
@@ -1,15 +1,31 @@
 @extends('layouts.master')
 
 @section('title')
-<h1>Reset Password</h1>
+<h2>Reset Password</h1>
 @stop
 
 @section('content')
-<form action="{{ action('RemindersController@postReset') }}" method="POST">
-    <input type="hidden" name="token" value="{{ $token }}">
-    <input type="email" name="email">
-    <input type="password" name="password">
-    <input type="password" name="password_confirmation">
-    <input type="submit" value="Reset Password">
-</form>
+
+<div class="row">
+ <div class="col-md-2 col-md-offset-8">
+   {{ Form::open(array('action' => 'RemindersController@postReset')) }}                       
+   
+   {{ Form::hidden('token', $token)}}
+
+   {{ Form::label('email', 'Email') }}
+   {{ Form::text('email', null, ['class' => 'form-control', 'autofocus' => 'autofocus']) }}
+   {{ $errors->first('email', '<span class="help-block">:message</span>') }}
+
+   {{ $errors->first('password', '<span class="help-block">:message</span>') }}    
+   {{ Form::label('password', 'Password') }}
+   {{ Form::password('password', ['class' => 'form-control']) }}
+
+   {{ $errors->first('password_confirmation', '<span class="help-block">:message</span>') }}
+   {{ Form::label('password_confirmation', 'Password Confirm') }}
+   {{ Form::password('password_confirmation', ['class' => 'form-control']) }}
+   <button class="btn btn-default" type="submit">Submit</button>
+   {{ Form::close() }}
+  </div>
+</div>
+
 @stop
