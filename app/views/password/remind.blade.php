d6836a833a73e8ed5462419cf79b5ed6d3853d1b
stanholzendorf <sholzendorf@mail.com>
2016-05-25 12:05:57 -0500

    Reminder

--- app/views/password/remind.blade.php
+++ app/views/password/remind.blade.php
@@ -1,14 +1,23 @@
 @extends('layouts.master')
 
 @section('title')
-<h1>Reminder Password</h1>
+<h2>Reminder Password</h1>
 @stop
 
 @section('content')
 
-<form action="{{ action('RemindersController@postRemind') }}" method="POST">
-    <input type="email" name="email">
-    <input type="submit" value="Send Reminder">
-</form>
+
+
+<div class="row">
+ <div class="col-md-2 col-md-offset-8">
+   {{ Form::open(array('action' => 'RemindersController@postRemind')) }}                       
+   {{ Form::label('email', 'Email') }}
+   {{ Form::text('email', null, ['class' => 'form-control', 'autofocus' => 'autofocus']) }}
+   {{ $errors->first('email', '<span class="help-block">:message</span>') }}
+   <button class="btn btn-default" type="submit">Submit</button>
+   {{ Form::close() }}
+  </div>
+</div>
+
 
 @stop
